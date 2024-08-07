b19b7eed86985d87685042c93daaa8c6034de37c
Luis791 <luisfernandoaguilera22@gmail.com>
2024-08-06 22:15:48 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value));
+});
+
+document.getElementById('borrar-todo').addEventListener('click', () => display.borrarTodo());
+document.getElementById('borrar').addEventListener('click', () => display.borrar());
